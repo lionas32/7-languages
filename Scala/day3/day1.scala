ffa669e7e8b7f2ece8b62621a17b51bcb411061c
Domantas Lionas <domantaslionas@hotmail.com>
2020-10-01 18:26:25 +0200

    scala: day 3 (not done)

---
+++ Scala/day3/day1.scala
@@ -0,0 +1,11 @@
+// Looks like the concurrency library used in the book is deprecated.
+// Need to read up on scala.concurrency if i want to use the standard library.
+// Checkout example_files/sizes.scala for the file referenced.
+
+// Take the sizer application and add a message to count the number of
+// links on the page.
+
+// Bonus problem: Make the sizer follow the links on a given page, and
+// load them as well. For example, a sizer for “google.com” would compute
+// the size for Google and all of the pages it links to.
+
