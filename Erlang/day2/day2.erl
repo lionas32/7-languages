e3560172e7cd79026c6a44901f16fe1b46d7ac62
Domantas Lionas <domantaslionas@hotmail.com>
2020-10-03 02:10:48 +0200

    erlang: day 2 bonus task

--- Erlang/day2/day2.erl
+++ Erlang/day2/day2.erl
@@ -4,6 +4,8 @@
 
 -export([calc_total_price/1]).
 
+-export([check_tic_tac_toe/1]).
+
 % Consider a list of keyword-value tuples, such as [{erlang, "a functional language"},
 % {ruby, "an OO language"}]. Write a function that accepts the list and
 % a keyword and returns the associated value for the keyword.
@@ -19,3 +21,29 @@ key_to_value(List, Key) ->
 calc_total_price(List) ->
     [{Item, Quantity * Price}
      || {Item, Quantity, Price} <- List].
+
+% Write a program that reads a tic-tac-toe board presented as a list or a
+% tuple of size nine. Return the winner (x or o) if a winner has been determined, cat if there are no more possible moves, or no_winner if no player
+% has won yet.
+check_rows([H1, H2, H3, H4, H5, H6, H7, H8, H9],
+           Player) ->
+    WonRow = fun (X) -> X == Player end,
+    lists:all(WonRow, [H1, H4, H7]) or
+        lists:all(WonRow, [H2, H5, H8])
+        or lists:all(WonRow, [H3, H6, H9]).
+
+check_columns([], _) -> false;
+check_columns([H1, H2, H3 | Tail], Player) ->
+    WonRow = fun (X) -> X == Player end,
+    lists:all(WonRow, [H1, H2, H3]) or
+        check_columns(Tail, Player).
+
+check_tic_tac_toe(List) ->
+    A = check_columns(List, o) or check_rows(List, o),
+    B = check_columns(List, x) or check_rows(List, x),
+    C = lists:any(fun (X) -> X == ' ' end, List),
+    if A -> o;
+       B -> x;
+       C -> no_winner;
+       true -> cat
+    end.
